2f8301c117b6359228afe7f275703daeb6c941d8
Nicolas da Rosa Moraes <darosamoraesn@gmail.com>
2021-02-24 12:18:44 -0300

    ChallengeBox finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { CompletedChallenges } from '../components/CompletedChallenges';
-import { Countdown } from '../components/Countdown';
-import { ExperienceBar } from '../components/ExperienceBar';
-import { Profile } from '../components/Profile';
+import Head from "next/head";
 
-import Head from 'next/head'
+import React from "react";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
-import styles from '../styles/pages/Home.module.css';
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
@@ -24,9 +25,9 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
-  )
+  );
 }
